765edb50075d6d7e45d39e2519868414ce7d3b40
vodelerk <lallamavioleta@gmail.com>
2019-02-14 21:06:24 -0500

    file organization

--- ASimpleWebServer/ASimpleWebServer.go
+++ ASimpleWebServer/ASimpleWebServer.go
@@ -2,21 +2,19 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", handler) // each request calls handler
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	//http.HandleFunc("/", handler) // each request calls handler
+	//log.Fatal(http.ListenAndServe("localhost:8000", nil))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		LissajousCreator(w)
+		lissajous(w)
 	})
 }
 
 // handler echoes the Path component of the request URL r.
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+//func handler(w http.ResponseWriter, r *http.Request) {
+//fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 
-}
+//}
