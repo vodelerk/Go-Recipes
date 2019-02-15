e715396328ce99fd52da411e68929e11b191e492
vodelerk <lallamavioleta@gmail.com>
2019-02-14 20:35:29 -0500

    unnneded

--- ASimpleWebServer/ASimpleWebServer.go
+++ ASimpleWebServer/ASimpleWebServer.go
@@ -10,9 +10,13 @@ import (
 func main() {
 	http.HandleFunc("/", handler) // each request calls handler
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		LissajousCreator(w)
+	})
 }
 
 // handler echoes the Path component of the request URL r.
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+
 }
