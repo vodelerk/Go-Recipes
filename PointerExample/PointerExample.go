32dfa1b2766eb4a0b80c6e0618ef1ddef08c52e1
vodelerk <lallamavioleta@gmail.com>
2019-02-15 14:02:53 -0500

    encapsulate method

--- PointerExample/PointerExample.go
+++ PointerExample/PointerExample.go
@@ -4,9 +4,14 @@ package main
 import "fmt"
 
 func main() {
+	simplepointer()
+}
+func simplepointer() {
 	x := 1
-	p := &x         // p, of type *int, points to x
-	fmt.Println(*p) // "1"
-	*p = 2          // equivalent to x = 2
-	fmt.Println(x)  // "2"
+	p := &x                                    // p, of type *int, points to x
+	fmt.Println(*p)                            // "1"
+	*p = 2                                     // equivalent to x = 2
+	fmt.Println(x)                             // "2"
+	var z, y int                               // pointer comparison
+	fmt.Println(&z == &z, &z == &y, &z == nil) //true false false
 }
