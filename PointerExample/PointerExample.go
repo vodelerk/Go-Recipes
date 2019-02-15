d8abc391bf9a3353b891dd75a6ce0fbed456ba7b
vodelerk <lallamavioleta@gmail.com>
2019-02-15 13:47:42 -0500

    pointer example

---
+++ PointerExample/PointerExample.go
@@ -0,0 +1,12 @@
+// simple pointer example
+package main
+
+import "fmt"
+
+func main() {
+	x := 1
+	p := &x         // p, of type *int, points to x
+	fmt.Println(*p) // "1"
+	*p = 2          // equivalent to x = 2
+	fmt.Println(x)  // "2"
+}
