45333c10207b685b6a8b157ec15269c7e7c7d383
vodelerk <lallamavioleta@gmail.com>
2019-02-15 14:15:58 -0500

    variations of echo with use of two flags

--- echo/echo.go
+++ echo/echo.go
@@ -2,11 +2,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
 func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
+
+func basicEcho() {
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
