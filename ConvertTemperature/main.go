5dc6b5415d6ff80c26327af8b6f9c7c67bb9bad9
vodelerk <lallamavioleta@gmail.com>
2019-02-17 19:57:18 -0500

    added tests

--- ConvertTemperature/main.go
+++ ConvertTemperature/main.go
@@ -8,4 +8,10 @@ func main() {
 	boilingF := CToF(BoilingC)
 	fmt.Printf("%g\n", boilingF-CToF(FreezingC)) // "180" °F
 	fmt.Printf("%g\n", boilingF-FreezingC)       // compile error: type mismatch
+	var c Celsius
+	var f Fahrenheit
+	fmt.Println(c == 0)          // "true"
+	fmt.Println(f >= 0)          // "true"
+	fmt.Println(c == f)          // compile error: type mismatch
+	fmt.Println(c == Celsius(f)) // "true"!
 }
