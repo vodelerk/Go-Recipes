26f009ae6544e764319ecf4ccd7a79c007a437a1
vodelerk <lallamavioleta@gmail.com>
2019-02-18 15:17:38 -0500

    added different prints

--- ConvertTemperature/main.go
+++ ConvertTemperature/main.go
@@ -14,4 +14,12 @@ func main() {
 	fmt.Println(f >= 0)          // "true"
 	fmt.Println(c == f)          // compile error: type mismatch
 	fmt.Println(c == Celsius(f)) // "true"!
+
+	c := FToC(212.0)
+	fmt.Println(c.String()) // "100°C"
+	fmt.Printf("%v\n", c)   // "100°C"; no need to call String explicitly
+	fmt.Printf("%s\n", c)   // "100°C"
+	fmt.Println(c)          // "100°C"
+	fmt.Printf("%g\n", c)   // "100"; does not call String
+	fmt.Println(float64(c)) // "100"; does not call String
 }
