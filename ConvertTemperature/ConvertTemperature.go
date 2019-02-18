3adb5394f3f2001b6b0c70228172707827aa2155
vodelerk <lallamavioleta@gmail.com>
2019-02-17 19:59:29 -0500

    other func test

--- ConvertTemperature/ConvertTemperature.go
+++ ConvertTemperature/ConvertTemperature.go
@@ -1,6 +1,8 @@
 // Package tempconv performs Celsius and Fahrenheit temperature computations.
 package tempconv
 
+import "fmt"
+
 type Celsius float64
 type Fahrenheit float64
 
@@ -10,5 +12,6 @@ const (
 	BoilingC      Celsius = 100
 )
 
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+func CToF(c Celsius) Fahrenheit  { return Fahrenheit(c*9/5 + 32) }
+func FToC(f Fahrenheit) Celsius  { return Celsius((f - 32) * 5 / 9) }
+func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
