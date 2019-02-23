76a910128350fd56db266b5ed968c032d0d263aa
vodelerk <lallamavioleta@gmail.com>
2019-02-23 16:08:11 -0500

    added hex printing example

--- BitWise/main.go
+++ BitWise/main.go
@@ -26,4 +26,13 @@ func main() {
 	for i := len(medals) - 1; i >= 0; i-- {
 		fmt.Println(medals[i]) // "bronze", "silver", "gold"
 	}
+
+	//formatting examples
+	o := 0666
+	fmt.Printf("%d %[1]o %#[1]o\n", o) // "438 666 0666"
+	xc := int64(0xdeadbeef)
+	fmt.Printf("%d %[1]x %#[1]x %#[1]X\n", xc)
+	// Output:
+	// 3735928559 deadbeef 0xdeadbeef 0XDEADBEEF
+
 }
